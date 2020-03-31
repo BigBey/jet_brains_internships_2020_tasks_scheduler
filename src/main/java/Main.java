23cfc8ef844d0add9312c217c3ea8b5fc29c999d
BigBey <bey.sviatoslav@yandex.ru>
2020-03-31 22:01:57 +0300

    TasksList implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import entities.TasksList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        TasksList list = new TasksList("List1");
+        list.addTask("Task1");
+        list.addTask("Task2");
+        list.addTask("Task3");
+        list.markTask(0);
+        list.showTasks();
     }
 }
--- src/main/java/entities/TasksList.java
+++ src/main/java/entities/TasksList.java
@@ -3,26 +3,59 @@ package entities;
 import java.util.ArrayList;
 
 public class TasksList {
-    private ArrayList tasks;
+    private ArrayList<Task> tasks;
     private String title;
 
-    public void addTask(String taskMessage){
-        //запись нового задания в список и в todo-list.json
+    public TasksList(String title) {
+        this.tasks = new ArrayList<>();
+        this.title = title;
     }
 
-    public void deleteTask(int index){
-        //удаление нового задания из списка и из todo-list.json
+    public void addTask(String taskMessage) {
+
+        tasks.add(new Task(taskMessage));
+        //запись в json
     }
 
-    public void markTask(int index){
-        //помечаем задание как выполненное/невыполненное
+    public void deleteTask(int index) {
+
+        tasks.remove(index);
+        //удаление из json
     }
 
-    public void showTasks(){
-        //выводим все задания
+    public void markTask(int index) {
+
+        Task task = tasks.get(index);
+
+        if (task.getCompleted()) {
+            tasks.get(index).setCompleted(false);
+        } else {
+            tasks.get(index).setCompleted(true);
+        }
     }
 
-    public void showOnlyNotCompletedTasks(){
-        //выводим еще не выполненные дела
+    public void showTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(i);
+            builder.append(". ");
+            builder.append(tasks.get(i).toString());
+            if(tasks.get(i).getCompleted()){
+                builder.append(" *");
+            }
+            System.out.println(builder.toString());
+        }
+    }
+
+    public void showOnlyNotCompletedTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            if(!tasks.get(i).getCompleted()) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(i);
+                builder.append(". ");
+                builder.append(tasks.get(i).toString());
+                System.out.println(builder.toString());
+            }
+        }
     }
 }
