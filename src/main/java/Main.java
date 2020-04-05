e61395181b73b7ce9963244dd6d3a730dd4d57b9
BigBey <bey.sviatoslav@yandex.ru>
2020-04-05 22:05:40 +0300

    Finished commands and tests.

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import menu.MenuImplementation;
-
-public class Main {
-    public static void main(String[] args) {
-        MenuImplementation menu = new MenuImplementation();
-        menu.deleteList("New List");
-    }
-}
